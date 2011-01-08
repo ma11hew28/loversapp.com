8a971c05f94a6cb31a969229c3eef78a79273838
Matt Di Pasquale <liveloveprosper@gmail.com>
2011-01-08 00:53:34 -0500

    implement redis reqs & rels

--- loversapp.rb
+++ loversapp.rb
@@ -1,3 +1,8 @@
+###############################################################################
+# Constants ###################################################################
+###############################################################################
+PERR = "x" # params validation error
+
 configure :development do
   require 'ruby-debug'
   REDIS = Redis.new
@@ -8,13 +13,49 @@ configure :production do
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
 
+before { content_type 'application/json' }
+
 get '/' do
+  content_type 'text/html'
   'Hello world!'
 end
 
+###############################################################################
+# Canvas ######################################################################
+###############################################################################
+
+# GET (serve) the Facebook app's canvas page.
 get '/fb/canvas/' do
+  content_type 'text/html'
+  cache_control :public, :max_age => 31536000 # seconds (1 year)
   erb :main
 end
+
+post '/fb/canvas/' do
+  content_type 'text/html'
+  "post response"
+end
+
+###############################################################################
+# Share #######################################################################
+###############################################################################
+# Facebook Stream API
+# - Show all my friends posts
+# - Show all posts
+
+
+###############################################################################
+# Requests ####################################################################
+###############################################################################
+# How can we manage requests on Facebook? If we can set a type for each sent
+# request and get & delete them, then we could replace this request code.
+#
+# Each user has two Redis ordered sets (`reqSent` & `reqRecv`) that store uids.
+# The SCORE that we order the requests by is `time` (UNIX timestamp).
+# The request id (rid) is encoded as a number and prepended to the uid. rid|uid
+# E.g., for the user with uid=100, we might have:
+#   100:reqSent => ["1|123", "2|123", "2|134"]  # format: ["rid|tid"]
+#   100:reqRecv => ["3|343", "5|142", "4|2224"] # format: ["rid|uid"]
 
 REL = {
   :"0" => "Relationship",
@@ -26,52 +67,95 @@ REL = {
 }
 NUM_RELS = REL.size
 
-# Return all requests for a user.
-# How can we store these requests using Facebook's request system?
+# GET (show) all requests for a user.
 get '/fb/canvas/reqs' do
   # Validate that uid is an integer.
-  uid = params[:uid] || "2"; # get this from session
-  return 0 unless (Integer(uid) rescue false)
-  REDIS.zrange(uid+':reqsRecv', 0, -1).inspect
+  uid = params[:uid] || "1" # get uid from FB
+  return PERR unless (Integer(uid) rescue false)
+  REDIS.zrange(uid+':reqRecv', 0, -1).inspect
 end
 
-# Add a request from one user (uid) to another (tid)
+# POST (add) a request from one user (uid) to another (tid)
 post '/fb/canvas/reqs' do
-  error = "0"
+  # Validate that uid, tid, rel are integers and rel is btw 0 to NUM_RELS.
+  uid, tid, rel = params[:uid], params[:tid], params[:rel] # get uid from FB
+  return PERR unless valid_rel?(rel)
+  return PERR unless (Integer(uid) && Integer(tid) rescue false)
 
-  # Validate relationship type.
-  rel = Integer(params[:rel]) rescue false
-  return error unless NUM_RELS > rel && rel > 0
-  rel = rel.to_s
+  # If my friend already made this request, confirm it.
 
-  # Validate that uid & tid are integers.
-  # TODO, get uid from FB auth.
-  uid = params[:uid]; tid = params[:tid]
-  return error unless (Integer(uid) && Integer(tid) rescue false)
+  # If this relation already exists, return w/ feedback code.
 
   # Add the request.
   now = Time.now.to_i
-  REDIS.zadd(uid+':reqsSent', now, rel+'|'+tid) # not shown but may in future
-  REDIS.zadd(tid+':reqsRecv', now, rel+'|'+uid)
+  REDIS.zadd(uid+':reqSent', now, rel+'|'+tid) # not shown but may in future
+  REDIS.zadd(tid+':reqRecv', now, rel+'|'+uid) ? "1" : "0"
 end
 
-# Ignore (delete) a request.
+# DELETE (ignore) a request.
 delete '/fb/canvas/reqs' do
-  error = "0"
-
-  # Validate relationship type.
-  rel = Integer(params[:rel]) rescue false
-  return error unless NUM_RELS > rel && rel > 0
-  rel = rel.to_s
-
-  # Validate that uid & tid are integers.
-  # TODO, get uid from FB auth.
-  uid = params[:uid]; tid = params[:tid]
-  return error unless (Integer(uid) && Integer(tid) rescue false)
+  # Validate that uid, tid, rel are integers and rel is btw 0 to NUM_RELS.
+  uid, tid, rel = params[:uid], params[:tid], params[:rel] # get uid from FB
+  return PERR unless valid_rel?(rel)
+  return PERR unless (Integer(uid) && Integer(tid) rescue false)
 
   # Remove the request.
-  REDIS.zrem(uid+':reqsSent', rel+'|'+tid) # not shown but may in future
-  REDIS.zrem(tid+':reqsRecv', rel+'|'+uid)
+  REDIS.zrem(tid+':reqSent', rel+'|'+uid) # not shown but may in future
+  REDIS.zrem(uid+':reqRecv', rel+'|'+tid) ? "1" : "0"
+end
+
+
+###############################################################################
+# Relationships ###############################################################
+###############################################################################
+
+# GET (show) all relationships for a user.
+get '/fb/canvas/rels' do
+  # Validate that uid is an integer.
+  uid = params[:uid] || "2"; # get this from session
+  return PERR unless (Integer(uid) rescue false)
+  REDIS.smembers(uid+':rels').inspect
+end
+
+# POST (confirm/add) a relationship between two users.
+post '/fb/canvas/rels' do
+  # Validate that uid, tid, rel are integers and rel is btw 0 to NUM_RELS.
+  uid, tid, rel = params[:uid], params[:tid], params[:rel] # get uid from FB
+  return PERR unless valid_rel?(rel)
+  return PERR unless (Integer(uid) && Integer(tid) rescue false)
+
+  # Remove the request, and add the relationship.
+  REDIS.zrem(tid+':reqSent', rel+'|'+uid) # not shown but may in future
+  if REDIS.zrem(uid+':reqRecv', rel+'|'+tid)
+    REDIS.sadd(uid+':rels', rel+'|'+tid) ||
+    REDIS.sadd(tid+':rels', rel+'|'+uid) ? "1" : "0"
+  else "o" end # no request
+end
+
+# DELETE (break up) a relationship.
+delete '/fb/canvas/rels' do
+  # Validate that uid, tid, rel are integers and rel is btw 0 to NUM_RELS.
+  uid, tid, rel = params[:uid], params[:tid], params[:rel] # get uid from FB
+  return PERR unless valid_rel?(rel)
+  return PERR unless (Integer(uid) && Integer(tid) rescue false)
+
+  # Remove the relationship.
+  REDIS.srem(uid+':rels', rel+'|'+tid) ||
+  REDIS.srem(tid+':rels', rel+'|'+uid) ? "1" : "0"
+end
+
+
+###############################################################################
+# Utility Methods #############################################################
+###############################################################################
+
+def valid_rel?(rel)
+  rel = Integer(rel) rescue (return false)
+  return NUM_RELS > rel && rel > 0
+end
+
+def valid_int?(str)
+  Integer(uid)
 end
 
 # Cool projects
@@ -85,10 +169,4 @@ end
 # Specs: It should:
 # Redis
 
-# Facebook Stream API
-# - Show all my friends posts
-# - Show all posts
-#
-
 # Questions
-# What is O(N) for SCARD? Is it better to store the CARD so that it's O(1)?
