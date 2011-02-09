76fb8a3838dd7a3766c6914220897c7d29779ed8
Matt Di Pasquale <liveloveprosper@gmail.com>
2011-02-09 08:06:27 -0500

    pass FB auth features: add NON_USER[:signed_request]

--- vendor/facebook/features/support/facebook_test.rb
+++ vendor/facebook/features/support/facebook_test.rb
@@ -9,11 +9,12 @@ class Facebook
   module Test
     APP = YAML.load_file("facebook.yml").symbolize_keys
     NON_USER = {
-      signed_request: "fsdfsdf.sldkjfsi" # TODO: get sample from FB
+      signed_request: "zSlZavxXafQ6vXTE4Z9QTwKbfQwIg2O_8OoQ9RDDULY.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImlzc3VlZF9hdCI6MTI5NzI1NjQzMywidXNlciI6eyJjb3VudHJ5IjoidXMiLCJsb2NhbGUiOiJlbl9VUyIsImFnZSI6eyJtaW4iOjIxfX19"
     }
     APP_USER = {
       # example signed_request Facebook sends via POST request
       signed_request: "FG1uGHoaGeNH2lxcfJG8AU1MBosRPTf_Wf6R5HQo-2Y.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImV4cGlyZXMiOjEyOTQ1MTY4MDAsImlzc3VlZF9hdCI6MTI5NDUxMjQ3Miwib2F1dGhfdG9rZW4iOiIxMjAwMjc3NDU0MzB8Mi5FS1NJd2FOZ21GN1c3aF9pTV9BM2Z3X18uMzYwMC4xMjk0NTE2ODAwLTUxNDQxN3xHV3dUT3FzWnI1S1pSUTBwVWFEMVB3MjhZSDgiLCJ1c2VyIjp7ImxvY2FsZSI6ImVuX1VTIiwiY291bnRyeSI6InVzIn0sInVzZXJfaWQiOiI1MTQ0MTcifQ",
+      # another_signed_request: "ez0cE0fg0d5PTvFSvbdPCxl2xkFBrZH3YGj9rgu74io.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImV4cGlyZXMiOjEyOTcyNjAwMDAsImlzc3VlZF9hdCI6MTI5NzI1NjIyOCwib2F1dGhfdG9rZW4iOiIxMjAwMjc3NDU0MzB8Mi54VFZIMjV0WWVZazBIb1ZPcmJta1J3X18uMzYwMC4xMjk3MjYwMDAwLTUxNDQxN3w1MkFYNmpaMWs3ZTNnWk5DRGgyRmVGMjBCV00iLCJ1c2VyIjp7ImNvdW50cnkiOiJ1cyIsImxvY2FsZSI6ImVuX1VTIiwiYWdlIjp7Im1pbiI6MjF9fSwidXNlcl9pZCI6IjUxNDQxNyJ9",
       user_id: "514417" # extracted from signed_request
     }
     MAL_USER = {
