75354a418f32783c2c76564205d95857b87160e9
Matt Di Pasquale <liveloveprosper@gmail.com>
2013-01-15 17:30:40 -0500

    Disable rack-protection.

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 
+configure do
+  # set :protection, :origin_whitelist => ['https://apps.facebook.com']
+  disable :protection
+end
+
 # Initial Facebook request comes in as a POST with a signed_request.
 post "/facebook/ " do
   erb :index
